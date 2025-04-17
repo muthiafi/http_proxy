4c6d526fb33b6a53f3581b76a4717e0e5d6c15f5
muthiafi <muthia.firdaus@kitalulus.com>
2025-04-17 09:48:14 +0700

    [master] - Remove Registrar usage from plugin - resolve fluter 3.29 compatibility

--- android/src/main/java/com/lm/http_proxy/HttpProxyPlugin.java
+++ android/src/main/java/com/lm/http_proxy/HttpProxyPlugin.java
@@ -15,25 +15,16 @@ public class HttpProxyPlugin implements FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
-
+  private static final String CHANNEL_NAME = "com.lm.http.proxy";
+  
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "com.lm.http.proxy");
-    channel.setMethodCallHandler(this);
+    setupChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), CHANNEL_NAME);
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "com.lm.http.proxy");
-    channel.setMethodCallHandler(new HttpProxyPlugin());
+  private void setupChannel(io.flutter.plugin.common.BinaryMessenger messenger, String channelName) {
+    channel = new MethodChannel(messenger, channelName);
+    channel.setMethodCallHandler(this);
   }
 
   @Override
@@ -44,6 +35,9 @@ public class HttpProxyPlugin implements FlutterPlugin, MethodCallHandler {
         break;
       case "getProxyPort":
         result.success(getProxyPort());
+        break;
+      default:
+        result.notImplemented();
         break;
     }
   }
