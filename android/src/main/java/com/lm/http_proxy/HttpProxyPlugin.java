d727f8d1d6c13f46fd4cdaa17890a85fe5818bb4
muthiafi <muthia.firdaus@kitalulus.com>
2025-04-17 11:00:43 +0700

    [master] Resolve Flutter 3.29 compatibility issue with PluginRegistry

--- android/src/main/java/com/lm/http_proxy/HttpProxyPlugin.java
+++ android/src/main/java/com/lm/http_proxy/HttpProxyPlugin.java
@@ -6,7 +6,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** HttpProxyPlugin */
 public class HttpProxyPlugin implements FlutterPlugin, MethodCallHandler {
